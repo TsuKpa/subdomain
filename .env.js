a0ac9de1a7b5197ff70a17f965e30130b591ec10
nam.nguyen <nam.nguyen@gss-sol.com>
2023-06-28 16:47:06 +0700

    first commit

---
+++ .env.js
@@ -0,0 +1,6 @@
+module.exports = {
+    ...process.env,
+    MONGODB_URI: 'mongodb+srv://dbuser:dbpassword@cluster0.5wt57d.mongodb.net/tutorial?retryWrites=true&w=majority',
+    DOMAIN: 'vietnoy.xyz',
+    PORT: 9000  
+};
